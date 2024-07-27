87af6e556718bf4a8ad12af1822c6a591ad58121
Krishna <gkrrish.11@gmail.com>
2024-07-27 16:33:06 +0530

    eureke server-client added

---
+++ src/main/java/com/poc/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.poc.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://14c0-49-204-9-224.ngrok-free.app/swagger-ui/index.html") // Add your ngrok URL here
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
